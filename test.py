d18b2cf2dac45ddfa0f6ba4fb38d713990f1882d
Axel Goichon <agoichon@3B5.42angouleme.fr>
2023-08-24 17:28:09 +0200

    push02

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import pygame
 
-res = (1024, 600)
+window_res = (1024, 600)
+blue_color = (89, 152, 255)
 
 pygame.init()
 pygame.display.set_caption("Test RoboTech")
-window_surface = pygame.display.set_mode(res)
+window_surface = pygame.display.set_mode(window_res)
+window_surface.fill(blue_color)
+
+pygame.display.flip()
+
 launched = True
+
 
 while launched:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+             launched = False
+        elif event.type == pygame.KEYDOWN and event.type == K_ESCAPE:
             launched = False
